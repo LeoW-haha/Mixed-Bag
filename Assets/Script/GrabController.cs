39f0849ef4557b5f347b01ea520413c9485fa6de
LeoW-haha <leo.west-kaenpao@student.uts.edu.au>
2025-04-02 02:36:57 +1100

    replacethe

---
+++ Assets/Script/GrabController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GrabController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform grabDetect;
+    public Transform boxHolder;
+    public float rayDist;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+        if (grabCheck.collider != null && grabCheck.collider.tag == "Pickable") {
+            if (Input.GetKey(KeyCode.G)) {
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            }
+            else {
+                grabCheck.collider.gameObject.transform.parent = null;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+    }
+}
