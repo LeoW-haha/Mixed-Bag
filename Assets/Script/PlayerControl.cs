cc5e8f9eaf82ecff980ca662d7fc42bb3aea175c
LeoW-haha <leo.west-kaenpao@student.uts.edu.au>
2025-04-01 13:24:40 +1100

    holding shit

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -7,6 +7,7 @@ public class PlayerCtrl :MonoBehaviour
     private Rigidbody2D rb;
     private float speedX;
     private float speedY;
+    private bool facingLeft = false;
 
     void Start ()
     {
@@ -17,10 +18,25 @@ public class PlayerCtrl :MonoBehaviour
     {
         speedX = Input.GetAxisRaw("Horizontal");
         speedY = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetAxisRaw("Horizontal") > 0 && facingLeft) {
+            Flip();
+            facingLeft = false;
+        }
+        if(Input.GetAxisRaw("Horizontal") < 0 && !facingLeft) {
+            Flip();
+            facingLeft = true;
+        }
     }
 
     void FixedUpdate ()
     {
         rb.linearVelocity = new Vector2(speedX * moveSpeed, speedY * moveSpeed);
+    }
+
+    private void Flip() {
+        var theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
